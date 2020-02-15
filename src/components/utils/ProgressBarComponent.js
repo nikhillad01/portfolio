2fa0894706cae870f45cad1629820f77c4af351a
nikhil <nikhil@navigus.in>
2020-02-15 19:29:04 +0530

    Removed unnecessary scripts, style sheets and imports

--- src/components/utils/ProgressBarComponent.js
+++ src/components/utils/ProgressBarComponent.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./ProgressBar.css";
 
 export const ProgressBar = props => {
-  console.log(props.percentage);
-
   return (
     <div className="progress-bar">
       <div className="filler" style={{ width: `${props.percentage}%` }} />
